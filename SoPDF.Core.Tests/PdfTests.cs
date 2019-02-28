201b0e30bced4e543afc83590ad178166e94f0a5
Brian Ding <brian_ding@outlook.com>
2019-02-28 10:05:55 +0800

    commit

--- SoPDF.Core.Tests/PdfTests.cs
+++ SoPDF.Core.Tests/PdfTests.cs
@@ -11,6 +11,8 @@ namespace SoPDF.Core.Tests
         {
             PdfDocument document = new PdfDocument("Hello World! Again!");
             document.Save("test.pdf");
+
+            System.IO.File.WriteAllText("a.txt", (double.Parse("5818.92") - double.Parse("6338.48")).ToString("f2"));
         }
     }
 }
